354b48894a6bffcc8b1c0bd9621a7f391713431a
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-10-12 03:09:50 -0300

    favicon updated

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({content}) {
       <Head>
         <title> {content.head.title} </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="robots" content="index, follow" />
       </Head>
 
